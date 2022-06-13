c0343d5ebcb86011ac66a2cb7494fc313b20404f
unknown <mutasimibrahim01@gmail.com>
2022-06-13 18:54:05 -0500

    minor changes

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-from unicodedata import name
-
-
 print("\nHello welcome to Mutasim's madLibs game. Have fun!\n")
 
-Name = input("Name: ")
+thename = input("Name: ")
 number = input("Age: ")
 sport = input("Sport: ")
 
-madlib = f"My name is {Name}. I am {number} years old. In my free time I like to play {sport}."
+madlib = f"My name is {thename}. I am {number} years old. In my free time I like to play {sport}."
 print(madlib)
