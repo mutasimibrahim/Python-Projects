53b5d7e877b499e15bfe2ee1385f7f4e7e02652a
unknown <mutasimibrahim01@gmail.com>
2022-06-13 18:52:20 -0500

    finished madLibs game

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-print ("hello world")
-print ("second line")
-print ("hello world jfdskl;fjdsalk;fjklsad;;")
-print ("hello hhhhhhhhhhhhhhhhhhhhh")
+from unicodedata import name
+
+
+print("\nHello welcome to Mutasim's madLibs game. Have fun!\n")
+
+Name = input("Name: ")
+number = input("Age: ")
+sport = input("Sport: ")
+
+madlib = f"My name is {Name}. I am {number} years old. In my free time I like to play {sport}."
+print(madlib)
